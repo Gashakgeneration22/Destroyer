68ed97412fd5b41c4050e2ac6153ca661a58f7c1
Gashak22 <97940348+Gashakgeneration22@users.noreply.github.com>
2023-05-01 14:31:18 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# GAK BAHAYA TA #
+# SH TERATE JAYA#
+
+## install sslh #
+wget https://raw.githubusercontent.com/Gashakgeneration22/Destroyer/main/sslh/sslh.sh && chmod +x sslh.sh && ./sslh.sh
+sleep 1
+
+## install stunnel5 ##
+wget raw.githubusercontent.com/Gashakgeneration22/Destroyer/main/stunnel5/stunnel5.sh && chmod +x stunnel5.sh && ./stunnel5.sh
+sleep 1
+
+## install ssh websocket ##
+wget https://raw.githubusercontent.com/Gashakgeneration22/Destroyer/main/websocket/ws-only.sh && chmod +x ws-only.sh && ./ws-only.sh
+sleep 1
+
